ad4c3ef0965a61148bb12447686d9733eb57c169
esti08-hu <estioame@gmail.com>
2024-02-04 07:35:49 +0300

    second commit

--- bookstable.php
+++ bookstable.php
@@ -1,4 +1,6 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <?php
 require 'includes/snippet.php';
 require 'includes/db-inc.php';
@@ -7,13 +9,26 @@ require 'includes/header.php';
 if (isset($_POST['del'])) {
     $id = sanitize(trim($_POST['id']));
 
-    $sql_del = "Delete from books where BookId = $id";
+    $sql_del = "DELETE FROM books WHERE bookId = $id";
     $error = false;
     $result = mysqli_query($conn, $sql_del);
 
     if ($result) {
         $error = true;
     }
+}
+$sqlCategories = "SELECT DISTINCT catagories FROM books";
+$resultCategories = mysqli_query($conn, $sqlCategories);
+
+if (isset($_GET['search'])) {
+    $searchTerm = sanitize(trim($_GET['search']));
+
+    $sqlSearch = "SELECT * FROM books WHERE bookId LIKE '%$searchTerm%'";
+    $resultSearch = mysqli_query($conn, $sqlSearch);
+} else {
+    $sql = "SELECT * FROM books";
+    $query = mysqli_query($conn, $sql);
+    $count = 1;
 }
 ?>
 
@@ -30,7 +45,7 @@ if (isset($_POST['del'])) {
             <strong style="font-size: 20px;">Books Table</strong>
         </div>
     </div>
-    <div class="container" style=" padding-bottom: 1rem; box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3); background-color: #fff; border-radius: 10px;">
+    <div class="container" style="padding-bottom: 1rem; box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3); background-color: #fff; border-radius: 10px;">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <?php if (isset($error) === true) { ?>
@@ -43,44 +58,97 @@ if (isset($_POST['del'])) {
                 <div class="row" style="margin: 1rem;">
                     <a style="margin-top: 1rem; width: 13rem;" href="addbook.php"><button style="width: 100%; " class="btn btn-success col-lg-3 col-md-4 col-sm-11 col-11" style="margin-left: 15px; margin-bottom:5px"><i class="fa-solid fa-plus"></i> Add Book</button></a>
                 </div>
-            </div>
-            <table class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        
-                        <th><center>#</center></th>
-                        <th>catagories</th>
-                        <th>NumberOfBook</th>
-                        
-                    </tr>
-                </thead>
-                <?php
-                $sql = "SELECT * FROM books";
-                $query = mysqli_query($conn, $sql);
-                $count =1;
-                $sqlCategories = "SELECT DISTINCT catagories FROM books";
-                $resultCategories = mysqli_query($conn, $sqlCategories);
 
+                <!-- Add the search form -->
+                <form action="" method="GET" style='width:25rem; margin-left:5px'>
+                    <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="search"  placeholder="Search by BookId">
+                        <div class="input-group-append">
+                            <button style="margin-left: 3px" class="btn btn-primary" type="submit">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <?php
+            if (isset($_GET['search'])) {
+                if (mysqli_num_rows($resultSearch) > 0) {
+                    echo '<div class="row" style="margin: 1rem;">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                <th><center>#</center></th>
+                                <th>BoookId</th>
+                                <th>BoookTitle</th>
+                                <th>author</th>
+        
+                                <th>publisherName</th>
+                                <th>catagories</th>
+                                <th>callNumber</th>
+                                <th>numberOfBook</th>
+                                </tr>
+                            </thead>
+                            <tbody>';
+
+                    $count = 1;
+                    while ($row = mysqli_fetch_assoc($resultSearch)) {
+                        echo '<tr>
+                                <td><center>' . $count++ . '</center></td>
+                                <td>' . $row['bookId'] . '</td>
+                                <td>' . $row['bookTitle'] . '</td>
+                                <td>' . $row['author'] . '</td>
+                                <td>' . $row['publisherName'] . '</td>
+                                <td>' . $row['catagories'] . '</td>
+                                <td>' . $row['callNumber'] . '</td>
+                                <td>' . $row['numOfBook'] . '</td>
+                            </tr>';
+                    }
+
+                    echo '</tbody>
+                        </table>
+                    </div>';
+                } else {
+                    echo '<div class="row" style="margin: 1rem;">
+                            <div class="alert alert-warning" role="alert">
+                                No results found for the search.
+                            </div>
+                        </div>';
+                }
+            } else
+             {
+                echo '<table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th><center>#</center></th>
+                            <th>Categories</th>
+                            <th>NumberOfBook</th>
+                        </tr>
+                    </thead>
+                    <tbody>';
+            
                 while ($categoryRow = mysqli_fetch_assoc($resultCategories)) {
                     $category = $categoryRow['catagories'];
-
+            
                     $sqlTotalQuantity = "SELECT SUM(numOfBook) AS totalQuantity FROM books WHERE catagories = '$category'";
                     $resultTotalQuantity = mysqli_query($conn, $sqlTotalQuantity);
                     $totalQuantity = mysqli_fetch_assoc($resultTotalQuantity)['totalQuantity'];
-                ?>
-                    <tr onclick="window.location.href = 'booksDetail.php?catagory=<?php echo urlencode($category); ?>';">
-                        <td><center><?php echo $count++?></center></td>
-                        <td><?php echo $category; ?></td>
-                        <td><?php echo $totalQuantity; ?></td>
-                    </tr>
-                <?php
+            
+                    echo '<tr onclick="window.location.href = \'booksDetail.php?catagory=' . urlencode($category) . '\';">
+                        <td><center>' . $count++ . '</center></td>
+                        <td>' . $category . '</td>
+                        <td>' . $totalQuantity . '</td>
+                    </tr>';
                 }
-                ?>
-            </table>
+            
+                echo '</tbody>
+                </table>';
+            }
+            ?>
         </div>
     </div>
+
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
 </body>
 
-</html>
+</html>';
--- viewstudents.php
+++ viewstudents.php
@@ -3,76 +3,156 @@ require 'includes/snippet.php';
 require 'includes/db-inc.php';
 require 'includes/header.php';
 
-if (isset($_POST['submit'])) {
-    $id = trim($_POST['del_btn']);
-    $sql = "DELETE FROM student WHERE studentId = '$id'";
-    $query = mysqli_query($conn, $sql);
-
-    if ($query) {
-        echo "<script>alert('Student Deleted!')</script>";
-    }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
+
 <head>
     <link rel="stylesheet" href="style.css">
 </head>
-<body  style="background: radial-gradient(#fff, #AED2FF)">
-<div class="container" style="box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3); background: radial-gradient(#FFF, #FFCC70); border-radius: 10px; margin-top: 6rem;">
-    <?php include "includes/nav.php"; ?>
 
-    <div class="alert col-lg-7 col-md-12 col-sm-12 col-xs-12 offset-lg-2 offset-md-0 offset-sm-1" style="text-align: center; margin-top: 70px;">
-        <span class="glyphicon glyphicon-book"></span>
-        <strong style="font-size: 20px;">Student Table</strong> 
+<body style="background: radial-gradient(#fff, #AED2FF)">
+    <div class="container" style="box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3); background: radial-gradient(#FFF, #FFCC70); border-radius: 10px; margin-top: 6rem;">
+        <?php include "includes/nav.php"; ?>
+
+        <div class="alert col-lg-7 col-md-12 col-sm-12 col-xs-12 offset-lg-2 offset-md-0 offset-sm-1" style="text-align: center; margin-top: 70px;">
+            <span class="glyphicon glyphicon-book"></span>
+            <strong style="font-size: 20px;">Student Table</strong>
+        </div>
     </div>
-</div>
 
-<div class="container" style=" padding-bottom: 1rem; box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3); background-color: #fff; border-radius: 10px;">
-    <div class="panel panel-default">
-    <div class="panel-heading">
+    <div class="container" style=" padding-bottom: 1rem; box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3); background-color: #fff; border-radius: 10px;">
+        <div class="panel panel-default">
+            <div class="panel-heading">
                 <div class="row" style="margin: 1rem;">
-                    <a style="margin-top: 1rem; width: 13rem;" href="addstudent.php"><button style="width: 100%; " class="btn btn-success col-lg-3 col-md-4 col-sm-11 col-11" style="margin-left: 15px; margin-bottom:5px"><i class="fa-solid fa-plus"></i>  Add Student</button></a>
+                    <a style="margin-top: 1rem; width: 13rem;" href="addstudent.php"><button style="width: 100%; " class="btn btn-success col-lg-3 col-md-4 col-sm-11 col-11" style="margin-left: 15px; margin-bottom:5px"><i class="fa-solid fa-plus"></i> Add Student</button></a>
                 </div>
             </div>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Id No</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Department</th>
-                    <th>Phone no.</th>
-                    <th><center>REMOVE</center></th>
-                </tr>
-            </thead>
-            <?php
-            $sql = "SELECT * FROM student";
-            $query = mysqli_query($conn, $sql);
-            $counter = 1;
-            while ($row = mysqli_fetch_assoc($query)) {
-            ?>
-                <tbody>
-                    <tr>
-                        <td><?php echo $counter++; ?></td>
-                        <td><?php echo $row['studentId'] ?></td>
-                        <td><?php echo $row['name'] ?></td>
-                        <td><?php echo $row['email'] ?></td>
-                        <td><?php echo $row['department'] ?></td>
-                        <td><?php echo $row['phoneNumber'] ?></td>
-                        <td>
-                            <form action="viewstudents.php" method="post">
-                                <input type="hidden" value="<?php echo $row['studentId']; ?>" name="del_btn">
-                                <center><button type="submit" name="submit" style="color: #fff;" class="btn btn-danger"><span class="fas fa-trash"></button></center>
-                            </form>
-                        </td>
-                    </tr>
-                </tbody>
-            <?php } ?>
-        </table>
-    </div>
-</div>
 
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
+            <!-- Add the search form and button -->
+            <div class="row" style='width:25rem; margin-left:5px'>
+                <form action="" method="GET">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="search" placeholder="Search by student Id">
+                        <div class="input-group-append">
+                            <button style='margin-left:3px'  class="btn btn-primary" type="submit">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <?php
+            // Check if the search form is submitted for the second table
+            if (isset($_GET['search'])) {
+                // Get the search term from the user
+                $searchTerm = $_GET['search'];
+
+                // Prepare the SQL query for the second table
+                $sqlSecondTable = "SELECT * FROM student WHERE studentId LIKE '%$searchTerm%'";
+
+                // Execute the query for the second table
+                $resultSecondTable = $conn->query($sqlSecondTable);
+
+                // Check if any results were found for the second table
+                if ($resultSecondTable->num_rows > 0) {
+                    echo '<div class="row" style="margin: 1rem;">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Id No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Department</th>
+                            <th>Phone no.</th>
+                            <th><center>REMOVE</center></th>
+                        </tr>
+                    </thead>
+                    <tbody>';
+                    $counter = 1;
+                    while ($row = $resultSecondTable->fetch_assoc()) {
+                        echo '<tr>
+                    <td>' . $counter++ . '</td>
+                    <td>' . $row['studentId'] . '</td>
+                    <td>' . $row['name'] . '</td>
+                    <td>' . $row['email'] . '</td>
+                    <td>' . $row['department'] . '</td>
+                    <td>' . $row['phoneNumber'] . '</td>
+                    <td>
+                        <form action="viewstudents.php" method="post">
+                            <input type="hidden" value="' . $row['studentId'] . '" name="del_btn">
+                            <center><button type="submit" name="submit" style="color: #fff;" class="btn btn-danger"><span class="fas fa-trash"></button></center>
+                        </form>
+                    </td>
+                </tr>';
+                    }
+                    echo '</tbody>
+                </table>
+              </div>';
+                } else {
+                    echo '<div class="row" style="margin: 1rem;">
+                <div class="alert alert-warning" role="alert">
+                    No results found for the second table.
+                </div>
+              </div>';
+                }
+            } else {
+                $sqlAllStudents = "SELECT * FROM student";
+                $resultAllStudents = $conn->query($sqlAllStudents);
+
+                if ($resultAllStudents->num_rows > 0) {
+                    echo '<div class="row" style="margin: 1rem;">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Id No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Department</th>
+                            <th>Phone no.</th>
+                            <th><center>REMOVE</center></th>
+                        </tr>
+                    </thead>
+                    <tbody>';
+                    $counter = 1;
+                    while ($row = $resultAllStudents->fetch_assoc()) {
+                        echo '<tr>
+                    <td>' . $counter++ . '</td>
+                    <td>' . $row['studentId'] . '</td>
+                    <td>' . $row['name'] . '</td>
+                    <td>' . $row['email'] . '</td>
+                    <td>' . $row['department'] . '</td>
+                    <td>' . $row['phoneNumber'] . '</td>
+                    <td>
+                        <form action="viewstudents.php" method="post">
+                            <input type="hidden" value="' . $row['studentId'] . '" name="del_btn">
+                            <center><button type="submit" name="submit" style="color: #fff;" class="btn btn-danger"><span class="fas fa-trash"></button></center>
+                        </form>
+                    </td>
+                </tr>';
+                    }
+                    echo '</tbody>
+                </table>
+              </div>';
+                } else {
+                    echo '<div class="row" style="margin: 1rem;">
+                <div class="alert alert-warning" role="alert">
+                    No students found.
+                </div>
+              </div>';
+                }
+            }
+
+            ?>
+
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
 </body>
+
 </html>
