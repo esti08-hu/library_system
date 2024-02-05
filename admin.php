c7e271780053d583ef63f5ae38e4b9a3791222d9
esti08-hu <estioame@gmail.com>
2024-02-05 09:55:21 +0300

    the admin dashbord is updated

--- admin.php
+++ admin.php
@@ -8,11 +8,42 @@ if (!(isset($_SESSION['auth']) && $_SESSION['auth'] === true)) {
 }
 require 'includes/db-inc.php';
 include 'includes/header.php';
-
 ?>
-<head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transition: transform 0.3s;
+    filter: brightness(0.8);
+    box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3);
+  }
+
+  a:hover {
+    transform: scale(1.1);
+    filter: brightness(1);
+  }
+
+  img {
+    width: 10rem;
+    padding: 5px;
+  }
+
+  .button-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+</style>
 </head>
+
 <body style="background: radial-gradient(#fff, #AED2FF)">
     <div class="container" style="box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.3); background: radial-gradient(#FFF, #FFCC70); border-radius: 10px; margin-top: 6rem;">
         <?php include 'includes/nav.php'; ?>
@@ -22,11 +53,33 @@ include 'includes/header.php';
         </div>
     </div>
     <div class="row" style="display: flex; align-items: center; justify-content: center;">
+    <a style="width: 12rem; margin: 5rem 5rem ; padding: 0;" href="bookstable.php">
+  <img src="img/books.png" alt="Books Icon">
+  <div class="button-container">
+    <button style="width: 10rem;" class="btn btn-success col-lg-3 col-md-4 col-sm-12 col-12 me-3 mb-3">
+      All Books
+    </button>
+  </div>
+</a>
 
-            <a style="width: 12rem; margin: 1rem 2rem ;" href="bookstable.php"> <button style="width: 100%; margin: 0;" class="btn btn-success col-lg-3 col-md-4 col-sm-12 col-12 me-3 mb-3"><span class="glyphicon"></span><i class="fa-solid fa-book"></i> All Books</button></a>
-            <a style="width: 12rem; margin: 1rem 2rem ;" href="viewstudents.php"><button style="width: 100%;" class="btn btn-success col-lg-3 col-md-4 col-sm-12 col-12 me-3 mb-3"><span class="glyphicon"></span><i class="fa-solid fa-book-open-reader"></i> Students List</button></a>
-            <a style="width: 12rem; margin: 1rem 2rem ;" href="borrowedbooks.php"><button style="width: 100%;" class="btn btn-success col-lg-3 col-md-4 col-sm-12 col-12 me-3 mb-3"><span class="glyphicon"></span><i class="fa-solid fa-book-open"></i> Borrowed Books</button></a>
-       
+<a style="width: 12rem; margin: 5rem 5rem ;" href="viewstudents.php">
+  <img src="img/students.png" alt="Students Icon">
+  <div class="button-container">
+    <button style="width: 10rem;" class="btn btn-success col-lg-3 col-md-4 col-sm-12 col-12 me-3 mb-3">
+      Students List
+    </button>
+  </div>
+</a>
+
+<a style="width: 12rem; margin: 5rem 5rem ;" href="borrowedbooks.php">
+  <img src="img/borrowedbooks.png" alt="Borrowed Books Icon">
+  <div class="button-container">
+    <button style="width: 10rem;" class="btn btn-success col-lg-3 col-md-4 col-sm-12 col-12 me-3 mb-3">
+      Borrowed Books
+    </button>
+  </div>
+</a>
+
     </div>
     <script src="js/jquery.js" type="text/javascript"></script>
     <script src="js/bootstrap.js" type="text/javascript"></script>
